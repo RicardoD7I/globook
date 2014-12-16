2d3bef7c99e43df50c78bf6d60ab8e4f05954acd
rut_cort_s <https://rut_cort_s@bitbucket.org/iclpdev/iclp-dev.git>
2014-12-15 23:57:41 -0300

    metodo get publicacion

--- routes/publicacion.js
+++ routes/publicacion.js
@@ -2,12 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
-  require
-});
-router.post('/', require("./publicacion/publicacionPost"));
-
+app.get('/Publicaciones',
+ require('./routes/publicacion/publicacionGet'));
 //routes.get('/');
 
 module.exports = router;
