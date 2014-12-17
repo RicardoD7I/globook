7b4175e162e5aba82c176ffa301f9d6a24d10df7
rut_cort_s <https://rut_cort_s@bitbucket.org/iclpdev/iclp-dev.git>
2014-12-17 18:51:32 -0300

    publicacion/publicacionGet.js

--- routes/publicacion.js
+++ routes/publicacion.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-app.get('/Publicaciones',
- require('./routes/publicacion/publicacionGet'));
+//router.get('/', function(req, res) {
+//  res.send('respond with a resource');
+//  require
+//});
+app.get('/publicaciones', require('./routes/publicacion/publicacionGet'));
+
+
+router.post('/', require("./publicacion/publicacion"));
+
 //routes.get('/');
 
 module.exports = router;
