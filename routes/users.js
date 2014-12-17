689d80d0bb887d1f38967032343bdf390af2095d
ignaciodiazmaurino <ignaciodiazmaurino@gmail.com>
2014-12-17 00:23:22 -0300

    Configuracion de la BBDD

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var user = require('../model/user');
 
 /* GET users listing. */
-router.get('/users', function(req, res) {
-	console.log('dentro del archivo users.js');
-	res.send('loguear usuario');
+
+var loginUser = function(req, res){
+	user.findOne({'userName': 'admin', 'password': 'admin'},function(err, userlogged){
+		if (err) {
+			console.log('ERROR: ' + err);
+		} else {
+			console.log (userlogged.userName);
+		}
+	});
+};
+
+
+router.POST('/authenticate', loginUser);
+
+	
 });
 
 /* POST loguear */
