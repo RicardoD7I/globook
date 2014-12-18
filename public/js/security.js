59e637dcc3840ca53f6d1bd293336a8b3b541a84
Mariela <blanco,mariela12@gmail.com>
2014-12-18 18:17:03 -0300

    agregando authentication

--- public/js/security.js
+++ public/js/security.js
@@ -3,21 +3,29 @@
   
   
 
-  // encontrar un parametro de la url
-  var pass = "";
-  var user = "";
+  // encontrar campos y sus valores
+  var pass = $("#passwordInput").val();
+  var user =  $("#usernameInput").val();
+
   // usar ajax
     $.ajax({
-      url:  "/authenticate"
+      url:  "/users/authenticate".
+      type : "POST",
+      data : {
+        username : user,
+        password : pass
+      }
     })
     .done(function(data  ) {
 
-        var passwordEsValido = data.isValid;
-
+      var passwordEsValido = data.isValid;
       
       // mostrar el div de comentarios
       if (passwordEsValido) {
+        amplify.store("userToken", data.token)
         $("#formulario").show();
+      } else {
+        amplify.store("userToken", null)
       }
 
 
