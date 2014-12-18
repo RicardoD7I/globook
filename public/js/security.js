318ac6cd5f7cc2973de70b58f3850414363fe382
Mariela <blanco,mariela12@gmail.com>
2014-12-18 18:24:17 -0300

    cambio de ajax en login

--- public/js/security.js
+++ public/js/security.js
@@ -1,36 +1,40 @@
 
  $(document).ready(function(){
   
-  
-
+  $("#loginButton").on("click", function(){
+    
   // encontrar campos y sus valores
-  var pass = $("#passwordInput").val();
-  var user =  $("#usernameInput").val();
+    var pass = $("#passwordInput").val();
+    var user =  $("#usernameInput").val();
 
-  // usar ajax
-    $.ajax({
-      url:  "/users/authenticate".
-      type : "POST",
-      data : {
-        username : user,
-        password : pass
-      }
-    })
-    .done(function(data  ) {
+    // usar ajax
+      $.ajax({
+        url:  "/users/authenticate".
+        type : "POST",
+        data : {
+          username : user,
+          password : pass
+        }
+      })
+      .done(function(data  ) {
 
-      var passwordEsValido = data.isValid;
-      
-      // mostrar el div de comentarios
-      if (passwordEsValido) {
-        amplify.store("userToken", data.token)
-        $("#formulario").show();
-      } else {
-        amplify.store("userToken", null)
-      }
+        var passwordEsValido = data.isValid;
+        
+        // mostrar el div de comentarios
+        if (passwordEsValido) {
+          amplify.store("userToken", data.token)
+          $("#formulario").show();
+        } else {
+          amplify.store("userToken", null)
+        }
 
 
-    });
+      });
 
+
+  });
+
+  
 
  
 
