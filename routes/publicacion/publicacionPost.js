fd5350aa94bacbcc2550e67dfa845283beafddf8
rut_cort_s <https://rut_cort_s@bitbucket.org/iclpdev/iclp-dev.git>
2014-12-23 23:24:02 -0300

    publicacion/publicacionPost.js definicion del metodo.(hecho por sebas), faltaria comprobar las rutas

--- routes/publicacion/publicacionPost.js
+++ routes/publicacion/publicacionPost.js
@@ -6,3 +6,33 @@ module.exports = function(req, res) {
 	res.json({})
 
 }
+/**
+module.exports = function(req, res) {
+	Usuario.finById(req.params.userName, function(err, user){
+        if(!err){
+            if(user.token == req.body.token){    
+                    var fechaAct = new Date();
+                    var pub = new Publicacion({
+                            comentario : req.body.comentario, 
+                            fecha : fechaAct
+                        });
+ 
+                    pub.save(function(err) {
+                        if(!err) {
+                            res.json({ estado : 'Alta exitosa'});
+                        }else{
+                            res.json({estado : "ERROR: 404"})
+                        }
+                    });
+ 
+            }else{
+                res.json({estado : "ERROR: 403"})
+            }
+ 
+        }else{
+            res.json({estado : "ERROR: 404"})
+        }
+ 
+    });
+}
+**/
