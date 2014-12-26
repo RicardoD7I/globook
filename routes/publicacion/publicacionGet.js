aad72da1290a4e4c681c14b8b06d1c9402e101c2
rut_cort_s <https://rut_cort_s@bitbucket.org/iclpdev/iclp-dev.git>
2014-12-25 22:50:04 -0300

    agregado la validacion del username a las publicaciones

--- routes/publicacion/publicacionGet.js
+++ routes/publicacion/publicacionGet.js
@@ -2,13 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 module.exports = function(req, res) {
-
-   Publicacion.find(function(err, pub){
+   Usuario.finById(req.params.userName, function(err, user){
+   
        if(!err){
-           res.render('/publicacion/publicacionGet', {
-           title: 'Publicaciones',
-           pub: pub
-       });
+       	    if(user.token == req.body.token){    
+       	            Publicacion.find(function(err, pub){
+                    res.render('/publicacion/publicacionGet', {
+                    title: 'Publicaciones',
+                    pub: pub
+                    });
+                    pub.save(function(err) {
+                       if(!err) {
+                           res.json({ estado : 'EXITO'});
+                       }else{
+                           res.json({estado : "ERROR: 404"})
+                       }
+                   });
+            }else{
+               res.json({estado : "ERROR: 403"}) //Si el usuario no existe o no tiene permisos
+           }
        }
        else{
        	    res.json({estado : "ERROR: 404"})
