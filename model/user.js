dcd71c166012312c8dff5f59ede98f2deec9c6fa
ignaciodiazmaurino <ignaciodiazmaurino@gmail.com>
2014-12-26 13:44:57 -0300

    modifiqué la estructura del usuario para poder guardar el nombre y la ruta de la imagen

--- model/user.js
+++ model/user.js
@@ -2,11 +2,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
+
+	name: String,
 	userName: String,
 	password: String,
-	token: String
+	token: String,
+	img: String
+
 });
 
 module.exports = mongoose.model('User', userSchema);
-
-//var User = mongoose.model('node_globook.usercollection');
