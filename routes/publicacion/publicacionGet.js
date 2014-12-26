7def1875bec4c737669498dc7a3a3038dcf5f8e6
rut_cort_s <https://rut_cort_s@bitbucket.org/iclpdev/iclp-dev.git>
2014-12-25 22:20:41 -0300

    publicacion/publicacionPost.js y publicacion/publicacionGet.js

--- routes/publicacion/publicacionGet.js
+++ routes/publicacion/publicacionGet.js
@@ -2,14 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 module.exports = function(req, res) {
-	// devolver datos
-	res.json({});
-	
-	// llamada a mongoDB con res.json({}); adentro:
-/*
-   publicacion.find(function(err, pub){
-       res.json({});
+
+   Publicacion.find(function(err, pub){
+       if(!err){
+            
+       }
+       else{
+       	    res.json({estado : "ERROR: 404"})
+       }
+
    });
-*/
+
 
 }
--- routes/publicacion/publicacionPost.js
+++ routes/publicacion/publicacionPost.js
@@ -1,38 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-module.exports = function(req, res) {
-	// devolver datos
-	res.json({})
 
-}
-/**
 module.exports = function(req, res) {
-	Usuario.finById(req.params.userName, function(err, user){
-        if(!err){
-            if(user.token == req.body.token){    
-                    var fechaAct = new Date();
-                    var pub = new Publicacion({
-                            comentario : req.body.comentario, 
-                            fecha : fechaAct
-                        });
- 
-                    pub.save(function(err) {
-                        if(!err) {
-                            res.json({ estado : 'Alta exitosa'});
-                        }else{
-                            res.json({estado : "ERROR: 404"})
-                        }
-                    });
- 
-            }else{
-                res.json({estado : "ERROR: 403"})
-            }
- 
-        }else{
-            res.json({estado : "ERROR: 404"})
-        }
- 
+       Usuario.finById(req.params.userName, function(err, user){
+       if(!err){
+           if(user.token == req.body.token){    
+                   var fechaAct = new Date();
+                   var pub = new Publicacion({
+                           texto : req.body.texto, 
+                           fecha : fechaAct,
+                           titulo : req.body.titulo
+
+                       });
+
+                   pub.save(function(err) {
+                       if(!err) {
+                           res.json({ estado : 'EXITO'});
+                       }else{
+                           res.json({estado : "ERROR: 404"})
+                       }
+                   });
+
+           }else{
+               res.json({estado : "ERROR: 403"})
+           }
+
+       }else{
+           res.json({estado : "ERROR: 404"})
+       }
+
     });
 }
-**/
