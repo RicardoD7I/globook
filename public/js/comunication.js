444a636afbb10c305b42cdd23e0dd0af90f58e8a
ricardod7i@hotmail.com <ricardo.vella@GLB-D0599.globant.com>
2014-12-29 07:16:48 -0300

    merging ignacio siri commit

--- public/js/comunication.js
+++ public/js/comunication.js
@@ -1,20 +1,14 @@
-var readyUserData = false;
-var readyPub = false;
+var readyUserData = new $.Deferred();
+var readyPub = new $.Deferred();
 
-// usar promises o timer
-
-amplify.subscribe("readyAjaxUsuario", function () {
-    if (readyPub) {
-        amplify.publish("pageLoaded");
-    } else {
-        readyUserData = true;
-    }
+amplify.subscribe("onUserReady", function () {
+    readyUserData.resolve();
 });
 
-amplify.subscribe("readyAjaxPublicaciones", function () {
-    if (readyUserData) {
-        amplify.publish("pageLoaded");
-    } else {
-        readyPub = true;
-    }
+amplify.subscribe("onPubReady", function () {
+    readyPub.resolve();
+});
+
+$.when(readyUserData, readyPub).done(function () {
+    amplify.publish("pageLoaded");
 });
